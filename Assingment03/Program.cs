c0ba1d3a305952ec78ac8ee22064b471e77e72fe
omare <omare@OMAR_ESAM>
2024-12-16 20:14:29 +0200

    Add project files.

---
+++ Assingment03/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Assingment03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
+            //Console.WriteLine("Enter Your Number: ");
+            //int.TryParse(Console.ReadLine(), out int Number);
+            //if (Number % 3 == 0 && Number % 4 == 0)
+            //    Console.WriteLine("Yes");
+            //else Console.WriteLine("No");
+            #endregion
+            #region Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //Console.WriteLine("Enter Your Num: ");
+            //int.TryParse(Console.ReadLine(), out int num);
+
+            //if (num < 0)
+            //{
+            //    Console.WriteLine("negative ");
+            //}
+            //else if (num > 0){
+
+            //    Console.WriteLine("positive");
+            //}
+
+            #endregion
+            #region 3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+
+            //Console.WriteLine("Enter Your Num1: ");
+            //int.TryParse(Console.ReadLine(), out int num1);
+
+            //Console.WriteLine("Enter Your Num2: ");
+            //int.TryParse(Console.ReadLine(), out int num2);
+
+            //Console.WriteLine("Enter Your Num3: ");
+            //int.TryParse(Console.ReadLine(), out int num3);
+
+            //int max = Math.Max(num1, Math.Max(num2, num3));
+            //int min = Math.Min(num1, Math.Min(num2, num3));
+
+            //Console.WriteLine($"Max:  {max}");
+            //Console.WriteLine($"Min: { min}");
+            #endregion
+            #region 4- Write a program that allows the user to insert an integer number then check If a number is even or odd.
+
+            //Console.WriteLine("Enter an Number");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //if (num % 2 == 0)
+            //    Console.WriteLine("Even");
+            //else if (num % 2 == 1)
+            //    Console.WriteLine("Odd");
+            #endregion
+            #region 5- Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+            //Console.WriteLine("Enter the char");
+            //Char character = Char.ToLower(Console.ReadLine()[0]);
+            //if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+            //{
+            //    Console.WriteLine("vowel");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("consonant");
+            //}
+            #endregion
+            #region  Write a program that allows the user to insert an integer then print a multiplication table up to 12
+            //Console.WriteLine("Enter Your Number");
+            //int.TryParse(Console.ReadLine(), out int number);
+
+            //for (int i = 1; i < 12; i++)
+            //{
+
+            //    Console.WriteLine(number * i);
+
+            //    if (i < 12)
+            //    {
+            //        Console.WriteLine(" ");
+            //    }
+            //}
+            #endregion
+            #region 8- Write a program that allows to user to insert number then print all even numbers between 1 to this number
+            //Console.WriteLine("Enter Your Number");
+            //int.TryParse(Console.ReadLine(), out int number);
+
+            //for (int i = 1; i < number; i++) {
+
+            //    if (i % 2 == 0)
+            //    {
+
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region 9- Write a program that takes two integers then prints the power.
+            //Console.WriteLine("enter Num1");
+            //int.TryParse(Console.ReadLine(), out int num1);
+            //Console.WriteLine("enter Num2");
+            //int.TryParse(Console.ReadLine(), out int num2);
+            //Console.WriteLine(Math.Pow(num1 , num2));
+            #endregion
+            #region 11- Write a program to input the month number and print the number of days in that month.
+            
+            //Console.WriteLine("Enter Month Number:");        
+            //if (int.TryParse(Console.ReadLine(), out int monthNumber))
+            //{
+            //    int daysInMonth;
+
+            //    switch (monthNumber)
+            //    {
+            //        case 1:
+            //        case 3:
+            //        case 5:
+            //        case 7:
+            //        case 8:
+            //        case 10:
+            //        case 12:
+            //            daysInMonth = 31;
+            //            break;
+            //        case 4:
+            //        case 6:
+            //        case 9:
+            //        case 11:
+            //            daysInMonth = 30;
+            //            break;
+            //        case 2:
+            //            daysInMonth = 28; 
+            //            break;
+            //        default:
+            //            Console.WriteLine("Please enter a number between 1 and 12.");
+            //            return;
+            //    }
+
+            //    Console.WriteLine($"Days in Month: {daysInMonth}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Please enter a valid integer.");
+            //}
+            #endregion
+
+
+
+        }
+    }
+}
